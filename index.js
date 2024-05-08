9cd8261c42a0bfcc031f204dd9908f70500ed95c
edulego <edulego@yahoo.com>
2024-05-08 17:38:22 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+var nombre = "Eduardo1234"; // String
+var apellido = 'Caizaluisa%&%*-'; // String
+var contador = 0; // int Entero valor numerico
+
+var hacerClic = function(){
+                    alert('Hola mundo');
+                }
+
+
+
+var tmp = document.getElementById('resultado'); // Paramatro 1 del tipo string
+
+
